9794176574f0c0ac0fe9af2343a7c05eb105882e
Lakshya7710 <72271387+Lakshya7710@users.noreply.github.com>
2020-12-02 22:29:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,23 @@
 function setup(){
-    canvas=createCanvas(550,450);
-    canvas.center();
+    canvas=createCanvas(500,450);
+    canvas.position(560,60);
+    video=createCapture(VIDEO);
+    video.size(500,500);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
 }
+
+function modelLoaded(){
+    console.log("model is loaded");
+}
+
+function draw(){
+    background("#969a97");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
+}
+
