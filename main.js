5c8a8b0ab5aa07012ee67f208e951f2633ebe216
Lakshya7710 <72271387+Lakshya7710@users.noreply.github.com>
2020-12-04 22:08:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+nose_x=0;
+nose_y=0;
+left_wristx=0;
+right_wristx=0;
+difference=0;
 function setup(){
     canvas=createCanvas(500,450);
     canvas.position(560,60);
@@ -13,11 +18,24 @@ function modelLoaded(){
 
 function draw(){
     background("#969a97");
+    fill("red");
+    stroke("red");
+    square(nose_x,nose_y,difference);
+    document.getElementById("square_side").innerHTML="The width and height of the square is "+difference + " px";
 }
 
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
+        nose_x=results[0].pose.nose.x;
+        nose_y=results[0].pose.nose.y;
+        console.log("nose.x="+ nose_x);
+        console.log( "nose.y="+nose_y);
+
+        left_wristx=results[0].pose.leftWrist.x;
+        right_wristx=results[0].pose.rightWrist.x;
+        difference= floor(left_wristx-right_wristx);
+        console.log("left wrist= "+left_wristx+" right wrist= "+right_wristx+" difference= "+difference);
     }
 }
 
